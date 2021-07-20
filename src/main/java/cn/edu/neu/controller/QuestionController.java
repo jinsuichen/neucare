a2608678fce3b9d179b78ad03ca247f550331906
FengLing <m13998337244@163.com>
2021-07-20 15:04:32 +0800

    问题管理、模板管理UI重构

--- src/main/java/cn/edu/neu/controller/QuestionController.java
+++ src/main/java/cn/edu/neu/controller/QuestionController.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionController {
+
     @FXML
     private TableView tableView;
     @FXML
--- src/main/java/cn/edu/neu/controller/TemplateController.java
+++ src/main/java/cn/edu/neu/controller/TemplateController.java
@@ -40,6 +40,7 @@ public class TemplateController {
     public Button deleteButton;
 
 
+
     public TemplateController() {
     }
 
